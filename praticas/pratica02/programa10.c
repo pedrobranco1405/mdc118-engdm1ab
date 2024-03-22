4ed17253311d8baaaee4cf9857111038c57e1f22
pedrobranco1405 <160501083+pedrobranco1405@users.noreply.github.com>
2024-03-22 19:32:45 +0000

    pratica02 q10

--- praticas/pratica02/programa10.c
+++ praticas/pratica02/programa10.c
@@ -4,5 +4,9 @@ int main() {
   int numero;
   printf("insira um numero inteiro: ");
   int ok = scanf("%d" , &numero);
-  
+
+  for (int i = 1; i <= 10; i++) {
+    printf("%d x %d = %d\n", numero, i, numero * i);
+    }
+  return 0;
 }
